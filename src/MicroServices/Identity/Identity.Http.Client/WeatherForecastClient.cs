7138f1fc5202c63bcf1b3dd7a9c92a03379ff0cb
NakashimaToshiki <80368114+NakashimaToshiki@users.noreply.github.com>
2022-06-14 19:54:56 +0900

    Identity.Http.Clientプロジェクトの追加

---
+++ src/MicroServices/Identity/Identity.Http.Client/WeatherForecastClient.cs
@@ -0,0 +1,17 @@
+﻿namespace Identity.Http.Client;
+
+public class WeatherForecastClient
+{
+    private readonly HttpClient _client;
+    private readonly string _scheme = "WeatherForecast";
+
+    public WeatherForecastClient(HttpClient client)
+    {
+        _client = client ?? throw new ArgumentNullException(nameof(client));
+    }
+
+    public async Task<IEnumerable<WeatherForecast>?> Gets()
+    {
+        return await _client.GetFromJsonAsync<IEnumerable<WeatherForecast>>($"{_scheme}");
+    }
+}
